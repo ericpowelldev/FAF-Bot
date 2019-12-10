a7503012193a7ee50bbb8ca909b5e7020afd5bd0
epowell7 <ericpowell777@gmail.com>
2019-12-09 19:07:39 -0500

    working on dice roll

--- commands/general/dice.js
+++ commands/general/dice.js
@@ -4,44 +4,29 @@ module.exports = {
     name: `dice`,
     aliases: [`r`, `roll`],
     category: `general`,
-    description: `Returns heads or tails based on a 50-50.`,
+    description: `Rolls a die based on an amount, number of sides, and modifier.`,
     run: async (client, message, args) => {
         if (message.deletable) message.delete();
 
-        const addThese = args.substring(
-            args.lastIndexOf(`+`) + 1,
-            args.lastIndexOf(`+`)
-        );
-        const addThese2 = args.substring(
-            args.lastIndexOf(`+`) + 1,
-            args.lastIndexOf(`-`)
-        );
+        if (args[0].includes(`d`)) {
+            const amount = args[0].split(`d`)[0];
+            const numArgs = args[0].split(`d`)[1];
+            const sides = numArgs.split(`+` || `-` || `*` || `/` || ``)[0];
+            const mods = numArgs.split(`+` || `-` || `*` || `/` || ``).shift();
+            console.log(amount);
+            console.log(numArgs);
+            console.log(sides);
+            console.log(mods);
 
-        const subThese = args.substring(
-            args.lastIndexOf(`-`) + 1,
-            args.lastIndexOf(`-`)
-        );
-        const subThese2 = args.substring(
-            args.lastIndexOf(`-`) + 1,
-            args.lastIndexOf(`+`)
-        );
-
-        if (args.includes(`d`)) {
-if (args.includes)
+            if (!isNaN(amount) && !isNaN(sides)) {
+                return message.reply(`valid dice format.`);
+            }
+            else {
+                return message.reply(`invalid dice format!`);
+            }
         }
         else {
             return message.reply(`invalid dice format!`);
         }
-        const arr = args.split(`d`);
-        const amount = arr[0];
-        const sides = arr[1];
-
-        const 
-
-        
-        if (rdmInt(0, 1) === 0)
-            return message.reply(`rolled a ${amount}d${sides}${} and got ${}!`);
-        else
-            return message.reply(`flipped a coin and got TAILS!`);
     }
 }
